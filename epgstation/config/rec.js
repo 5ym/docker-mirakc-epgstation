1a499ba9a6ea45c828c1f13a27faa408e3cea8c8
5ym <16yuim@gmail.com>
2020-02-09 20:19:49 +0900

    update

--- epgstation/config/rec.js
+++ epgstation/config/rec.js
@@ -1,7 +1,7 @@
 var request = require('request');
 
 h2z = str => {
-    return str.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function(s) {
+    return str.replace(/[！-～]/g, function(s) {
         return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
     });
 }
@@ -12,8 +12,8 @@ request({
     headers: {'Content-Type': 'application/json'},
     body: JSON.stringify({value1:
 `${process.argv[2]}
-Title:${process.env.NAME}:${process.env.PROGRAMID}
-Channel:${process.env.CHANNELNAME?h2z(process.env.CHANNELNAME):process.env.CHANNELNAME}:${process.env.CHANNELID}
+Title:${process.env.NAME}
+Channel:${process.env.CHANNELNAME?h2z(process.env.CHANNELNAME):process.env.CHANNELNAME}
 Duration:${process.env.DURATION/1000/60}m
 Description:${process.env.DESCRIPTION}
 @5yuim`})
