d27a421e5b2d8b774412f1b54222c9cab713a0d5
5ym <16yuim@gmail.com>
2021-07-05 00:30:35 +0000

    update config

--- epgstation/config/rec.js
+++ epgstation/config/rec.js
@@ -11,13 +11,12 @@ request({
     url: 'https://api.daco.dev/w?key=dm5nT4nshQ1KxZwFv4A9EQ',
     method: 'POST',
     headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify({text:
-`${process.argv[2]}
+    body: {text: `${process.argv[2]}
 Title:${process.env.NAME?h2z(process.env.NAME):process.env.NAME}
 Channel:${process.env.CHANNELNAME?h2z(process.env.CHANNELNAME):process.env.CHANNELNAME}
 Duration:${process.env.DURATION/1000/60}m
 Description:${process.env.DESCRIPTION}
-@5yuim`})
+@5yuim`}
 }, (error, response, body) => {
     if (!error && response.statusCode == 200) {
         console.log(body);
