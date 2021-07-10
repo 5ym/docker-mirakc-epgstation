4084fe91894786c544226fb1ac8eee274af09b8c
5ym <16yuim@gmail.com>
2021-07-10 18:51:23 +0000

    update command

--- epgstation/config/rec.js
+++ epgstation/config/rec.js
@@ -7,17 +7,24 @@ h2z = str => {
     return tmp.replace(/　/g," ");
 }
 
+head = "The recording has been updated.";
+
+switch(process.argv[2]) {
+    case 'add':
+        head = "The recording has been added.";
+        break;
+}
+
 request({
-    url: 'https://api.daco.dev/w?key=dm5nT4nshQ1KxZwFv4A9EQ',
+    url: 'https://api.daco.dev/w/?key=dm5nT4nshQ1KxZwFv4A9EQ',
     method: 'POST',
     headers: {'Content-Type': 'application/json'},
-    body: {text: `${process.argv[2]}
+    body: JSON.stringify({text: `${head}
 Title:${process.env.NAME?h2z(process.env.NAME):process.env.NAME}
 Channel:${process.env.CHANNELNAME?h2z(process.env.CHANNELNAME):process.env.CHANNELNAME}
 Duration:${process.env.DURATION/1000/60}m
 Description:${process.env.DESCRIPTION}
-@5yuim`}
-}, (error, response, body) => {
+@5yuim`})}, (error, response, body) => {
     if (!error && response.statusCode == 200) {
         console.log(body);
     } else {
